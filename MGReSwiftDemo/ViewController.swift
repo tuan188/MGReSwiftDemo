ad728bb6bea1a4132a62b761c5e7ab9ff3f025d4
Tuan Truong <tuan188@gmail.com>
2017-02-20 14:14:07 +0700

    Init project

---
+++ MGReSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MGReSwiftDemo
+//
+//  Created by Tuan Truong on 2/20/17.
+//  Copyright © 2017 Tuan Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
